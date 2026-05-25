0201d29901ba8b0c49f87a25c970d70a65368aa0
aymen197 <aymenmuhammed2@gmail.com>
2026-05-25 14:12:51 +0300

    Update Motorcycle getInfo and add FuelType

--- src/Fuel_Station/Motorcycle.java
+++ src/Fuel_Station/Motorcycle.java
@@ -1,4 +1,21 @@
 package Fuel_Station;
 
-public class Motorcycle {
+public class Motorcycle extends Vehicle {
+    private String fuelType;
+
+    public Motorcycle(String ownerName, String PlateNumber, String vehicleType) {
+        super(ownerName, PlateNumber, vehicleType);
+        this.fuelType = fuelType;
+
+    }
+    public String getfuelType() {
+        return fuelType;
+    }
+    @Override
+    public String getInfo(){
+        return "Motorcycle | Owner: " + getOwnerName() + " | Plate: " + getPlatenumber() + " | Fuel: " + fuelType;
+
+    }
+
 }
+
